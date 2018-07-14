158abce1dc56c077a6af8a42e6da98de653c9bf7
Muiz Yusuff <muiz.project@gmail.com>
2018-07-13 21:30:16 -0400

    Added Ball controller to project

---
+++ Assets/BallCtrl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCtrl : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ( collision.gameObject.tag == "Ground" )
+        {
+            // end game when ball hits the ground
+            GameObject.Find("Game Controller").GetComponent<GameController>().gameOver();
+            this.gameObject.SetActive(false);
+        }
+    }
+
+}
